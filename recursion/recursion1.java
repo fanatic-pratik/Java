a22e0b0f42a67d0a7246622b45f6b37e23ab6605
fanatic-pratik <pratikdhamal6@gmail.com>
2026-01-30 12:40:32 +0530

    recursion class 1 and fixed java compiler and runtime versions

--- recursion/recursion1.java
+++ recursion/recursion1.java
@@ -9,8 +9,20 @@ public class recursion1 {
         printNum(n-1);
     }
 
+    public static void printSum(int n , int i, int sum) {
+        if(n == i){
+            sum +=i;
+            System.out.println(sum);
+            return;
+        }
+
+        sum +=i;
+        printSum(n,i+1,sum);
+    }
+
     public static void main(String[] args) {
         int n = 5;
-        printNum(n);
+        //printNum(n);
+        printSum(n, 1, 0);
     }
 }
